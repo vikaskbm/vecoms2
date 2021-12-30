ea85e40eaa64d0ed98029a6fe9f9441e498c31e2
Vikas Bishnoi <vikaskbm@gmail.com>
2021-12-31 05:21:42 +0530

    Add vecom to allowed host

--- vecom/settings.py
+++ vecom/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vecoms.herokuapp.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
